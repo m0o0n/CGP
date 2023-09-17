5ddf3bfb6df62f15a86b8867a96bf4d9957c8af1
m0o0n <strikunvlad@gmail.com>
2023-09-17 16:50:30 +0300

    ref: refactor constructorReducer

--- src/store/constructorReducer.js
+++ src/store/constructorReducer.js
@@ -1,10 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
     blocks: [
-        { id: 1, order: 1, type: "image", value: null },
-        { id: 2, order: 2, type: "headline", value: "asdasffasffasfa casdasdasd asd sd" },
-        { id: 3, order: 3, type: "paragraph", value: "asdasffasffasfa casdasdasd asd sd" },
-        { id: 4, order: 4, type: "button", value: "submit" },
+        { id: 1, type: "image", value: null },
+        { id: 2, type: "headline", value: "Lorem Impsum" },
+        { id: 3, type: "paragraph", value: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus eaque totam beatae, omnis tenetur deserunt eveniet unde at cupiditate quae quas aliquid exercitationem facere corrupti eos. Veniam voluptate totam autem!" },
+        { id: 4, type: "button", value: "submit" },
     ],
     onDragCurrent: null
 }
@@ -13,42 +13,31 @@ const constructorReducer = createSlice({
     initialState,
     reducers: {
         setBlocks: (state, action) => {
-            console.log(action.payload)
             state.blocks = [...action.payload]
         },
-        addBlock: (state, action) => {
-            console.log(action.payload)
-            state.blocks = [...state.blocks, action.payload]
-        },
         editBlock: (state, action) => {
-            console.log(action.payload)
             const index =state.blocks.indexOf(state.blocks.find((e) => e.id === action.payload.id)) 
             state.blocks[index] = {...state.blocks[index], value: action.payload.value}
         },
         copyBlock: (state, action) => {
-            console.log(action.payload)
             state.blocks = [
                 ...state.blocks,
                 {...state.blocks.find(e => e.id === action.payload), id: (new Date()).getTime()}
             ]
         },
-
         deleteBlock: (state, action) => {
-            console.log(action.payload)
             state.blocks = [
                 ...state.blocks.filter(e => e.id !== action.payload)
             ]
         },
         setDragCurrent: (state, action) => {
-            console.log(state, action)
             const date = new Date()
-            state.onDragCurrent = { id: date.getTime(), order: date.getTime(), type: action.payload, value: '' }
+            state.onDragCurrent = { id: date.getTime(), type: action.payload, value: '' }
         }
     }
 })
 export const {
     setBlocks,
-    addBlock, 
     editBlock,
     copyBlock,
     deleteBlock,
