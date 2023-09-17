ea443822842357a937f05f0e9ad1e7ba524eca89
m0o0n <strikunvlad@gmail.com>
2023-09-17 16:48:17 +0300

    fix: resolve replacment

--- src/components/workSpace/workSpace.jsx
+++ src/components/workSpace/workSpace.jsx
@@ -1,14 +1,23 @@
 import * as React from 'react';
 import { TextWidget } from '../widgets/textWidget';
 import { useSelector, useDispatch } from 'react-redux';
-import { addBlock, setDragCurrent } from '../../store/constructorReducer';
+import { addBlock, setBlocks, setDragCurrent } from '../../store/constructorReducer';
 import { FileWidget } from '../widgets/fileWidget';
-
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 const WorkSpace = ({ onDrop, onDragOver }) => {
     const data = useSelector(state => state.Constructor)
+    useEffect(()=>{
+        setItems(data.blocks)
+    }, [data.blocks])
+
+    useEffect(()=>{
+        setCurrentItem(data.onDragCurrent)
+    }, [data.onDragCurrent])
+
+
     const dispatch = useDispatch()
-    console.log(data)
     const isActiveHandler = (e) => {
         if (e.target.className === "widget") {
             e.target.parentNode.classList.add('work_space__item__active')
@@ -21,16 +30,50 @@ const WorkSpace = ({ onDrop, onDragOver }) => {
             })
         }
     }
-    
+    const [items, setItems] = useState(data.blocks)
+    const [currentItem, setCurrentItem] = useState(null)
+    const dragStartHandler = (e, target) => {
+
+        console.log('dragStartHandler', target)
+        const currentItemInArray = items.find((el) => el.id === Number(target.dataset.id))
+        console.log(target)
+        setCurrentItem(currentItemInArray)
+        
+    }
+    const dragEndHandler = (e) => {
+        e.preventDefault()
+        console.log('dragEndHandler')
+        e.currentTarget.style.opacity = '1'
+    }
+    const dragOverHandler = (e) => {
+        e.preventDefault()
+        e.currentTarget.style.opacity = '0'
+        if(currentItem){
+            const overedId = Number(e.currentTarget.dataset.id)
+            const indexOfOvered = items.indexOf(items.find(e=> e.id === overedId))
+            const copy = [...items.filter(e=> e.id !== currentItem.id)]
+            copy.splice(indexOfOvered, 0, currentItem)
+            setItems(copy)
+            if(overedId !== currentItem.id){
+                dispatch(setBlocks(copy))
+            }
+        }
+        
+    }
+    const onDropHandler = (e, target) => {
+        e.preventDefault()
+        e.currentTarget.style.opacity = '1'
+        console.log('onDropHandler', target, currentItem)
+    }
     return (
 
         <div
             className='work_space'
             onDrop={(e) => {
                 e.preventDefault()
-                if(data.onDragCurrent && data.onDragCurrent.type){
-                    dispatch(addBlock(data.onDragCurrent))  
+                if (data.onDragCurrent && data.onDragCurrent.type) {
                     dispatch(setDragCurrent(null))
+                    console.log(items)
                 }
             }}
             onDragOver={(e) => {
@@ -48,13 +91,20 @@ const WorkSpace = ({ onDrop, onDragOver }) => {
                                 key={el.id}
                                 id={el.id}
                                 type={el.type}
-                             />
+                                dragStartHandler={dragStartHandler}
+                                dragEndHandler={dragEndHandler}
+                                dragOverHandler={dragOverHandler}
+                                onDropHandler={onDropHandler}
+                            />
                             : <TextWidget
                                 key={el.id}
                                 id={el.id}
                                 textValue={el.value}
                                 type={el.type}
-                                
+                                dragStartHandler={dragStartHandler}
+                                dragEndHandler={dragEndHandler}
+                                dragOverHandler={dragOverHandler}
+                                onDropHandler={onDropHandler}
                             />
                     )
                 })
